0bf22abb9fd96c9fac6699b38a39e15e54f3ba3a
David Ballesteros <david_ballesteros@outlook.com>
2024-04-19 22:55:50 +0800

    first commit

---
+++ first-app/_prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+   printWidth: 80,
+   tabWidth: 2,
+   useTabs: true,
+   semi: true,
+   singleQuote: false,
+   trailingComma: 'all', // 'es5' or 'none'
+   bracketSpacing: true,
+   arrowParens: 'avoid', // 'always'
+   parser: 'typescript',
+};
---
+++ first-app/src/app/home/home.component.ts
@@ -0,0 +1,63 @@
+import { Component, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
+      </form>
+    </section>
+    <p class="results-count">{{ resultsCount }}</p>
+    <section class="results">
+      <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
+    </section>
+  `,
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  filteredLocationList: HousingLocation[] = [];
+
+  resultsCount: string = "";
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()) ||
+        housingLocation?.name.toLowerCase().includes(text.toLowerCase())
+    );
+
+    if (this.filteredLocationList.length == 0 && (text.length == 0 || text == undefined)) {
+      this.resultsCount = `No results for search term: ${text}`;
+    } else {
+      this.resultsCount = `Showing ${this.filteredLocationList.length} of ${this.housingLocationList.length} listings.`;
+    }
+  }
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+
+    this.resultsCount = `Showing ${this.filteredLocationList.length} of ${this.housingLocationList.length} listings.`;
+  }
+}
